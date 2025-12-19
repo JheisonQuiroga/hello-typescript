b6f84e8c44700f84311fdeaeac14f9bc8869eee4
JheisonQuiroga <jheisonquiroga7@gmail.com>
2025-12-19 13:54:13 -0500

    initialize project with TypeScript configuration and basic setup

---
+++ Basics/01-basic-types.ts
@@ -0,0 +1,15 @@
+const name: string = "Duban";
+let hpPoints: number | 'FULL' = 100;
+let isAlive: boolean = true;
+
+hpPoints = 'FULL';
+
+
+console.log({
+  name,
+  hpPoints,
+  isAlive,
+});
+
+// Se utiliza el export para convertir el archivo en un módulo.
+export {};
