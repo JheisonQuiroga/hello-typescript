fe78b73e8678f2b927cc56e2e79746fde9ed5180
JheisonQuiroga <jheisonquiroga7@gmail.com>
2025-12-27 20:04:07 -0500

    add function destructuring example and update main script to include it

---
+++ Basics/05-function-destructuring.ts
@@ -0,0 +1,47 @@
+/// Function destructuring
+
+interface Product {
+  description: string;
+  price: number;
+}
+
+const phone: Product = {
+  description: 'Iphone 17 Pro Max',
+  price: 1999.9,
+}
+
+const ipad: Product = {
+  description: 'Ipad Air',
+  price: 800.0,
+}
+
+const shoppingCart: Product[] = [phone, ipad];
+const tax: number = 0.19;
+
+interface TaxCalculationOptions {
+  tax: number;
+  products: Product[];
+}
+
+function taxCalculation( options: TaxCalculationOptions ): [number, number] {
+  
+  const { tax, products } = options;
+  
+  let total = 0;
+
+  products.forEach( ({ price }) => {
+    total += price;
+  })
+
+  return [total, total * tax];
+}
+
+const result: number[] = taxCalculation( { tax, products: shoppingCart})
+const [total, taxTotal] = result;
+
+console.log(`Total: ${total}`);
+console.log(`Tax: ${taxTotal}`);
+
+console.log({result});
+
+export {};
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
 // import '../Basics/02-object-interface';
 // import '../Basics/03-functions';
 import '../Basics/04-object-destructuring';
+import '../Basics/05-function-destructuring';
