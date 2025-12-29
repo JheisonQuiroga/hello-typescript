c203762b781d9c640d7e74a58ef47dfa1a78dc3b
JheisonQuiroga <jheisonquiroga7@gmail.com>
2025-12-29 16:57:06 -0500

    refactor Person class to include lastName property; update Hero class to use composition

--- Basics/07-classes.ts
+++ Basics/07-classes.ts
@@ -2,12 +2,14 @@ export class Person {
 
   // Properties
   public name?: string;
+  public lastName: string;
   // es lo mismo a age?: number;
   private age: number | undefined;
 
   // Constructor
-  constructor( name: string, age?: number ) {
+  constructor( name: string, lastName:string, age?: number ) {
     this.name = name;
+    this.lastName = lastName;
     this.age = age || 0;
   }
 
@@ -19,10 +21,26 @@ export class Person {
 
 // Inheritance
 
-class Hero extends Person {
+// class Hero extends Person {
 
-  constructor(private power: string, public realName: string, age?: number ) {
-    super( realName, age );
+//   constructor(private power: string, public realName: string, age?: number ) {
+//     super( realName, age );
+//     this.power = power;
+//   }
+
+//   public showPower(): string {
+//     return `My power is ${ this.power}`;
+//   }
+// }
+
+// Composition over Inheritance
+
+class Hero {
+
+  public person: Person;
+
+  constructor(private power: string, public realName: string, person: Person, age?: number ) {
+    this.person = person;
     this.power = power;
   }
 
@@ -31,11 +49,11 @@ class Hero extends Person {
   }
 }
 
-const bruceWayne = new Person('Bruce Wayne');
-console.log(bruceWayne.name);
+const tony = new Person('Tony', 'Stark', 45);
+console.log(tony.name);
 // console.log(bruceWayne.age);
-console.log(bruceWayne.greet());
+console.log(tony.greet());
 
-const ironman = new Hero('Technology', 'Ironman', 45);
-console.log(ironman.greet());
+const ironman = new Hero('Technology', 'Ironman', tony, 45);
+// console.log(ironman.greet());
 console.log(ironman.showPower());
