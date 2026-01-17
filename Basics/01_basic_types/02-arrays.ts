c4cb3bd6e23669c7f416e0ef4940dbdd57ce27cb
JheisonQuiroga <jheisonquiroga7@gmail.com>
2026-01-17 17:36:26 -0500

    add: expand array examples in 02-arrays.ts with various typing methods

--- Basics/01_basic_types/02-arrays.ts
+++ Basics/01_basic_types/02-arrays.ts
@@ -1,2 +1,15 @@
 /// Arrays
 
+// Formas de tipar un Array
+
+const numbers: number[] = [1, 2, 3, 4, 5];
+const numbers2: Array<number> = [1, 2, 3, 4, 5];
+
+const strings: string[] = ['a', 'b', 'c', 'd', 'e'];
+const strings2: Array<string> = ['a'];
+
+// Multiples tipos de datos en un Array
+
+const variable: (string | number | boolean)[] = ["a", 2, true];
+const variable2: Array<string | number | boolean>[] = ["a", 2, true];
+
