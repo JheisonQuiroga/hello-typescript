f5d74e4f790d8d6f6be888aa469c81ee12324ce3
JheisonQuiroga <jheisonquiroga7@gmail.com>
2026-01-17 14:24:29 -0500

    fix: enhance type safety by adding noImplicitAny option in tsconfig and update variable declarations in 01-basic-types.ts

--- Basics/01-basic-types.ts
+++ Basics/01-basic-types.ts
@@ -1,10 +1,45 @@
 import './02-object-interface.js';
 
+
+// Strings
+// Una constante tiene que estar inicializada de lo contrario marcara un error
 const name: string = "Duban";
+const lastname: string = 'Quiroga';
+// const nickname; // Esto marca un error
+
+
+console.log(`I'm ${name} ${lastname}`);
+
+/// Utilizando el Optional chaining
+console.log(lastname[10]?.toUpperCase() || 'Not found')
+
+// Numbers
 let hpPoints: number | 'FULL' = 100;
+
+// Boolean
 let isAlive: boolean = true;
 
 hpPoints = 'FULL';
+
+// Any
+/*  Es un tipo de dato que no es muy recomendable utilizar. 
+    Una variable declarada con 'let' que NO ES INICIALIZADA por lo general
+    queda de tipo 'any'
+*/
+
+let superHeroName: any = 'Batman';
+let nickname; /* Variable implicita de tipo any - undefined */
+
+/// Casteo de tipos de datos
+/* El casteo es un trato de tipo de dato superficial, es decir tratar un tipo de dato
+    como otro tipo de dato, sin realmente alterarlo. */
+
+superHeroName = 123;
+superHeroName = 123.34343423;
+(superHeroName as number).toFixed(2);
+(<string>superHeroName).charAt(0);
+
+nickname = 'The Dark Knight';
 
 
 console.log({
