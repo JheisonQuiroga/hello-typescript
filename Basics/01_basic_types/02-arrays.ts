f70a925b854a7ce42cf07fcc150c42dd32b0443b
JheisonQuiroga <jheisonquiroga7@gmail.com>
2026-03-04 10:34:32 -0500

    add: implement array return function and destructuring example

--- Basics/01_basic_types/02-arrays.ts
+++ Basics/01_basic_types/02-arrays.ts
@@ -20,3 +20,15 @@ const variable2: Array<string | number | boolean> = ["a", 2, true, "b", 3, false
 const fruits: string[] = ['apple', 'banana', 'strawberry'];
 
 fruits.forEach((fruit) => console.log(fruit.toUpperCase()));
+
+// 1.2. Returning an Array from a function
+/* Retornando un array como una constante en una función, esto hace que 
+el tipo del array sea inferido automáticamente por TypeScript. */
+
+function getData() {
+  return ["Duban", 27] as const; // El uso de 'as const' hace que el array sea inmutable y sus tipos sean literales
+}
+
+// Array destructuring
+const [name, age] = getData();
+console.log(age + 100);
